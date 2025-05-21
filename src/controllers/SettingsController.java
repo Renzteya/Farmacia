b092d4218e8eead852fa96173ebe1647b860f585
Renzo Luque <renzoluquechino1@gmail.com>
2025-05-20 19:24:35 -0500

    feat(profile): mostrar datos del usuario y permitir cambio de contraseña

--- src/controllers/SettingsController.java
+++ src/controllers/SettingsController.java
@@ -1,13 +1,16 @@
-
 package controllers;
 
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import static models.EmployeesDao.address_user;
+import static models.EmployeesDao.email_user;
+import static models.EmployeesDao.full_name_user;
+import static models.EmployeesDao.id_user;
+import static models.EmployeesDao.telephone_user;
 import views.SystemView;
 
-
-public class SettingsController implements MouseListener {
+public class SettingsController implements MouseListener{
 
     private SystemView views;
     
@@ -20,66 +23,78 @@ public class SettingsController implements MouseListener {
         this.views.jLabelSuppliers.addMouseListener(this);
         this.views.jLabelCategories.addMouseListener(this);
         this.views.jLabelReports.addMouseListener(this);
-        this.views.jLabelSettigns.addMouseListener(this);
+        this.views.jLabelSettings.addMouseListener(this);
+        Profile();
     }
     
+    //Asignar el perfil del usuario
+    public void Profile(){
+        this.views.txt_Id_profile.setText(""+id_user);
+        this.views.txt_name_profile.setText(full_name_user);
+        this.views.txt_address_profile.setText(address_user);
+        this.views.txt_phone_profile.setText(telephone_user);
+        this.views.txt_email_profile.setText(email_user);
+    }
     
     @Override
     public void mouseClicked(MouseEvent e) {
-        
+       
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
+        
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        
-        if (e.getSource() == views.jLabelProducts){
-            views.jPanelProducts.setBackground(new Color(152, 202, 63));
-        }else if (e.getSource() == views.jLabelPurchases){
-            views.jPanelPurchases.setBackground(new Color(152, 202, 63));
-        }else if (e.getSource() == views.jLabelCustomers){
-            views.jPanelCustomers.setBackground(new Color(152, 202, 63));
-        }else if (e.getSource() == views.jLabelEmployees){
-            views.jPanelEmployees.setBackground(new Color(152, 202, 63));
-        }else if (e.getSource() == views.jLabelSuppliers){
-            views.jPanelSuppliers.setBackground(new Color(152, 202, 63));
-        }else if (e.getSource() == views.jLabelCategories){
-            views.jPanelCategories.setBackground(new Color(152, 202, 63));
-        }else if (e.getSource() == views.jLabelReports){
-            views.jPanelReports.setBackground(new Color(152, 202, 63));
-        }else if (e.getSource() == views.jLabelSettigns){
-            views.jPanelSettigns.setBackground(new Color(152, 202, 63));
-        }
-        
-        
+       if(e.getSource() == views.jLabelProducts){
+           views.jPanelProducts.setBackground(new Color(152, 202, 63));
+       }else if(e.getSource() == views.jLabelPurchases){
+           views.jPanelPurchases.setBackground(new Color(152, 202, 63));
+       }else if(e.getSource() == views.jLabelCustomers){
+           views.jPanelCustomers.setBackground(new Color(152, 202, 63));
+       }else if(e.getSource() == views.jLabelEmployees){
+           views.jPanelEmployees.setBackground(new Color(152, 202, 63));
+       }else if(e.getSource() == views.jLabelSuppliers){
+           views.jPanelSuppliers.setBackground(new Color(152, 202, 63));
+       }else if(e.getSource() == views.jLabelSuppliers){
+           views.jPanelSuppliers.setBackground(new Color(152, 202, 63));
+       }else if(e.getSource() == views.jLabelCategories){
+           views.jPanelCategories.setBackground(new Color(152, 202, 63));
+       }else if(e.getSource() == views.jLabelReports){
+           views.jPanelReports.setBackground(new Color(152, 202, 63));
+       }else if(e.getSource() == views.jLabelSettings){
+           views.jPanelSettings.setBackground(new Color(152, 202, 63));
+       }
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        if (e.getSource() == views.jLabelProducts){
-            views.jPanelProducts.setBackground(new Color(18, 45, 61));
-        }else if (e.getSource() == views.jLabelPurchases){
-            views.jPanelPurchases.setBackground(new Color(18, 45, 61));
-        }else if (e.getSource() == views.jLabelCustomers){
-            views.jPanelCustomers.setBackground(new Color(18, 45, 61));
-        }else if (e.getSource() == views.jLabelEmployees){
-            views.jPanelEmployees.setBackground(new Color(18, 45, 61));
-        }else if (e.getSource() == views.jLabelSuppliers){
-            views.jPanelSuppliers.setBackground(new Color(18, 45, 61));
-        }else if (e.getSource() == views.jLabelCategories){
-            views.jPanelCategories.setBackground(new Color(18, 45, 61));
-        }else if (e.getSource() == views.jLabelReports){
-            views.jPanelReports.setBackground(new Color(18, 45, 61));
-        }else if (e.getSource() == views.jLabelSettigns){
-            views.jPanelSettigns.setBackground(new Color(18, 45, 61));
-        }
+        if(e.getSource() == views.jLabelProducts){
+           views.jPanelProducts.setBackground(new Color(18,45,61));
+       }else if(e.getSource() == views.jLabelPurchases){
+           views.jPanelPurchases.setBackground(new Color(18,45,61));
+       }else if(e.getSource() == views.jLabelCustomers){
+           views.jPanelCustomers.setBackground(new Color(18,45,61));
+       }else if(e.getSource() == views.jLabelEmployees){
+           views.jPanelEmployees.setBackground(new Color(18,45,61));
+       }else if(e.getSource() == views.jLabelSuppliers){
+           views.jPanelSuppliers.setBackground(new Color(18,45,61));
+       }else if(e.getSource() == views.jLabelSuppliers){
+           views.jPanelSuppliers.setBackground(new Color(18,45,61));
+       }else if(e.getSource() == views.jLabelCategories){
+           views.jPanelCategories.setBackground(new Color(18,45,61));
+       }else if(e.getSource() == views.jLabelReports){
+           views.jPanelReports.setBackground(new Color(18,45,61));
+       }else if(e.getSource() == views.jLabelSettings){
+           views.jPanelSettings.setBackground(new Color(18,45,61));
+       }
     }
     
 }
